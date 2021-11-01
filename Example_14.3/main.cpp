5bc9b38ed0d9c80bd64c1d01dabee2342be82744
Yleb22 <80745115+Yleb22@users.noreply.github.com>
2021-11-01 17:33:27 +0200

    Update main.cpp

--- Example_14.3/main.cpp
+++ Example_14.3/main.cpp
@@ -17,6 +17,21 @@ int main() {
 	c2.Set(3333);
 	d = c1.Get(); // d = 3333; c1 і c2 вказують на ту саму ділянку пам'яті
 	cout << "d3 = " << d << endl;
+
+	cout << endl;
+
+	//EXAMPLE 14.4
+	CYear* pc = new CYear(); // покажчик, виділяється пам'ять
+	CYear& rc = (*pc); // посилання на пам'ять, виділену для покажчика
+	// доступ до об'єкту за покажчиком
+	pc->Set(2040);
+	d = pc->Get(); // d = 2040
+	cout << "d1 = " << d << endl;
+	d = rc.Get(); // d = 2040
+	cout << "d2 = " << d << endl;
+	rc.Set(2100);
+	d = pc->Get(); // d = 2100
+	cout << "d3 = " << d << endl;
 	system("pause");
 	return 0;
 }
