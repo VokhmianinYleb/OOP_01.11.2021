a86e1cf1235885d0bdbe1aaa0f43fdc67835c11b
Yleb22 <80745115+Yleb22@users.noreply.github.com>
2021-11-01 17:21:21 +0200

    Create MyPixelClass.h

---
+++ Example_14.2/MyPixelClass.h
@@ -0,0 +1,12 @@
+class MyPixelClass
+{
+	int x;
+	int y;
+	int color;
+public:
+	MyPixelClass(void);
+	int GetX(void);
+	int GetY(void);
+	int GetColor(void);
+	void SetXYColor(int nx, int ny, int nc);
+};
