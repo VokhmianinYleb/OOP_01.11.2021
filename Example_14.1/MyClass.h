ff3a3596e3b33cd08daef5eb46e363a8cc16aca2
Yleb22 <80745115+Yleb22@users.noreply.github.com>
2021-11-01 17:18:34 +0200

    Create MyClass.h

---
+++ Example_14.1/MyClass.h
@@ -0,0 +1,13 @@
+// опис класу в модулі "MyClass.h"
+class MyClass
+{
+private:
+	int x; // поля класу
+	int y;
+public:
+	// методи класу
+	void SetXY(int nx, int ny);
+	int GetX(void);
+	int GetY(void);
+	MyClass(void);
+};
